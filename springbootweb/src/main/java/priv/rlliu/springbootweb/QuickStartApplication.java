f0dd4788bcf8f2b0c86c803c363ce5e955cf99d4
Martin <Martin@DESKTOP-EUDQ3JC>
2018-12-21 17:26:45 +0800

    新增EurekaClient配置

--- springbootweb/src/main/java/priv/rlliu/springbootweb/QuickStartApplication.java
+++ springbootweb/src/main/java/priv/rlliu/springbootweb/QuickStartApplication.java
@@ -2,6 +2,7 @@ package priv.rlliu.springbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Spring Boot 应用启动类
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by rlliu on 26/09/2017.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class QuickStartApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuickStartApplication.class, args);
